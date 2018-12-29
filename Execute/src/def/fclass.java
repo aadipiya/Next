77c30b1b2d7d598393bb8169fca030cfee2fec69
aadipiya <sayli.gumte@gmail.com>
2018-12-29 11:09:34 +0530

    gjgj

--- Execute/src/def/fclass.java
+++ Execute/src/def/fclass.java
@@ -2,23 +2,5 @@ package def;
 
 public class fclass {
 
-<<<<<<< HEAD
-
-	public void eat()
-	{
-		System.out.println("ex");
-		System.out.println("ex");
-		
-		System.out.println("ex");
-		System.out.println("ex");
-	}
-
-=======
-	public void eat()
-	{
-		System.out.println("ex");
-	}
-	
-	
->>>>>>> branch 'master' of https://github.com/aadipiya/Next.git
 }
+
