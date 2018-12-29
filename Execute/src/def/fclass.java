23ec13fd8b2d8b5888151dd6a116cc1378c837e8
aadipiya <sayli.gumte@gmail.com>
2018-12-29 11:10:47 +0530

    jkhjk

--- Execute/src/def/fclass.java
+++ Execute/src/def/fclass.java
@@ -1,6 +1,14 @@
 package def;
 
 public class fclass {
-
+	public static void main(String[] args)
+	{
+		
+		System.out.println("piya");
+		System.out.println("piya");
+		System.out.println("piya");
+		
+		
+	}
 }
 
